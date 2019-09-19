d54eee74b5e711f5b20cdb2ecf557473babbd851
chunhongZheng <824394795@qq.com>
2019-09-19 23:09:24 +0800

    增加@propertySource注解及@value,@ConfigurationProperties的使用

---
+++ src/main/java/com/example/springboot/bean/Order.java
@@ -0,0 +1,95 @@
+package com.example.springboot.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@PropertySource(value = {"classpath:order.properties"})
+@ConfigurationProperties(prefix = "order")
+public class Order
+{
+    private String orderNo;
+    private String contNo;
+    private String prtNo;
+    private double amount;
+    private Date createDate;
+    private Date modifyDate;
+    private String createUser;
+    private String modifyuser;
+    private Integer isDelete;
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getContNo() {
+        return contNo;
+    }
+
+    public void setContNo(String contNo) {
+        this.contNo = contNo;
+    }
+
+    public String getPrtNo() {
+        return prtNo;
+    }
+
+    public void setPrtNo(String prtNo) {
+        this.prtNo = prtNo;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getModifyuser() {
+        return modifyuser;
+    }
+
+    public void setModifyuser(String modifyuser) {
+        this.modifyuser = modifyuser;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+}
--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,18 +1,38 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.bean.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-    @ResponseBody
+
     @RequestMapping("/hello")
     String home() {
         return  "Hello World!";
     }
+    /**
+     * 取配置文件某个属性值时用@value，要获取整个配置对象则使用@ConfigurationProperties
+     *
+     * ***/
+    @Value("${person.last-name}")
+    private String name;
 
+    @RequestMapping("/sayHello")
+    public String sayHello(){
+       return "say hello to "+name;
+    }
+    @Autowired
+    Order order;
 
+    @RequestMapping("/testPropertySource")
+    public String testPropertySource(){
+        return "orderno is :"+order.getOrderNo()+" and contNo is :"+order.getContNo();
+    }
 
 
 }
