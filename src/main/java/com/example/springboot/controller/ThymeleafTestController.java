2c14b86d0a60a3b440eb0e2a6a76ba422d8cc307
chunhongZheng <824394795@qq.com>
2019-09-25 23:09:07 +0800

    thymeleaf 测试

--- src/main/java/com/example/springboot/controller/ThymeleafTestController.java
+++ src/main/java/com/example/springboot/controller/ThymeleafTestController.java
@@ -3,11 +3,14 @@ package com.example.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class ThymeleafTestController {
     @RequestMapping("leaf/testThymeleaf")
-    public String testThymeleaf(){
+    public String testThymeleaf(Map<Object,Object> map){
         //classpath:/templates/success.html
+        map.put("hello","您好");
         return "success";
     }
 }
